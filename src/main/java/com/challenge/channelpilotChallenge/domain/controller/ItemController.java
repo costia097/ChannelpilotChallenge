c78eb8d52bb5309179f6bf637adf882a54898130
Kostia Yeriomin <kyer@ciklum.com>
2021-04-07 14:55:55 +0300

    fix paths

--- src/main/java/com/challenge/channelpilotChallenge/domain/controller/ItemController.java
+++ src/main/java/com/challenge/channelpilotChallenge/domain/controller/ItemController.java
@@ -3,11 +3,13 @@ package com.challenge.channelpilotChallenge.domain.controller;
 import com.challenge.channelpilotChallenge.domain.service.ItemService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/item")
 public class ItemController {
     private final ItemService itemService;
 
--- src/main/java/com/challenge/channelpilotChallenge/domain/controller/StoryController.java
+++ src/main/java/com/challenge/channelpilotChallenge/domain/controller/StoryController.java
@@ -5,12 +5,14 @@ import com.challenge.channelpilotChallenge.domain.service.StoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/story")
 public class StoryController {
     private final StoryService storyService;
 
